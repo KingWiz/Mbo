7c2d523ac214609ced81a13a6cd77ccfc47a271b
KIST Student3 <student3@sddb.student>
2022-03-11 09:02:57 +0545

    initial commit

---
+++ Mbo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Mbo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Mbo/Controllers/PrashilController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mbo.Models;
+
+namespace Mbo.Controllers
+{
+    public class PrashilController : Controller
+    {
+        MBOEntities db = new MBOEntities();
+        // GET: Default
+        public ActionResult Services()
+        {
+            List<Employee> all_data = db.Employees.ToList();
+            
+            return View(all_data);
+        }
+    }
+}
