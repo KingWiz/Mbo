ddfccc30daf79aadcbf4aa29fd0cd2e3b766fe75
amTsoL <boyrocking24@gmail.com>
2022-03-13 09:17:22 +0545

    third commit

--- Mbo/Controllers/PrashilController.cs
+++ Mbo/Controllers/PrashilController.cs
@@ -11,11 +11,21 @@ namespace Mbo.Controllers
     {
         MBOEntities db = new MBOEntities();
         // GET: Default
-        public ActionResult Services()
+        public ActionResult FormWork()
         {
             List<Employee> all_data = db.Employees.ToList();
             
             return View(all_data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult SaveData(Employee Employee)
+        {
+            db.Employees.Add(Employee);
+            db.SaveChanges();
+            return RedirectToAction("FormWork");
         }
     }
 }
---
+++ Mbo/Controllers/StdTableController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mbo.Models;
+
+namespace Mbo.Controllers
+{
+    public class StdTableController : Controller
+    {
+        MBOEntities db = new MBOEntities();
+        // GET: StdTable
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        
+    }
+}
